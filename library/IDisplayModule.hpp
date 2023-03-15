1a20cec8aa333a886e536f815d95b84799b9b018
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-15 21:33:25 +0100

    [ADD]: ENUM for storing all object type of graphical class and init function for creating the window before loading every game

--- library/IDisplayModule.hpp
+++ library/IDisplayModule.hpp
@@ -7,12 +7,24 @@
 
 #pragma once
 #include <string>
+#include <string>
+#include <ncurses.h>
+#include <map>
+
+enum class ObjectType {
+    PLAYER,
+    PLAYER_PART,
+    ENEMY,
+    ITEM,
+    BORDER
+};
 
 class IDisplayModule {
     public:
         virtual ~IDisplayModule() = default;
         virtual void init() = 0;
-        virtual void stop() = 0;
+        virtual void displayObjects(std::map<int, std::pair<ObjectType, std::pair<int, int>>> _ObjectData) = 0;
+        virtual void displayScore(int _Score) = 0;
         virtual const std::string &getName() const = 0;
 
     protected:
