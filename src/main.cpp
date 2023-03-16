1ec463487428dbd88e4dac27bbcc95d215eaaca7
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-16 09:17:40 +0100

    [ADD]: temp usage of init class in main.cpp file with comment for explaining

--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,29 @@
 
 static std::map<int, std::pair<ObjectType, std::pair<int, int>>> GameSimulation(int keypressed)
 {
-    std::map<int, std::pair<ObjectType, std::pair<int, int>>> _ObjectData;
+    std::map<int, std::pair<ObjectType, std::pair<int, int>>> _ObjectData;                      //create a map of objects with their positions
     (void)(keypressed);
 
-    _ObjectData[0] = std::make_pair(ObjectType::PLAYER, std::make_pair(10,  20));
-    _ObjectData[1] = std::make_pair(ObjectType::ENEMY, std::make_pair(5, 20));
-    _ObjectData[3] = std::make_pair(ObjectType::ITEM, std::make_pair(8, 30));
-    _ObjectData[4] = std::make_pair(ObjectType::ITEM, std::make_pair(10, 60));
+    _ObjectData[0] = std::make_pair(ObjectType::PLAYER, std::make_pair(10,  20));               //create a player at position 10, 20
+    _ObjectData[1] = std::make_pair(ObjectType::ENEMY, std::make_pair(5, 20));                  //create an enemy at position 5, 20
+    _ObjectData[3] = std::make_pair(ObjectType::ITEM, std::make_pair(8, 30));                   //create an item at position 8, 30
+    _ObjectData[4] = std::make_pair(ObjectType::ITEM, std::make_pair(10, 60));                  //create an item at position 10, 60
 
-    return _ObjectData;
+    return _ObjectData;                                                                         //return the map
 }
 
 int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
-    std::shared_ptr<IInit> _Init = std::make_shared<Init>("lib/");
-    std::cout << _Init->getGraphicalInstances()[0]->getInstance()->getName() << std::endl;
-    _Init->getGraphicalInstances()[0]->getInstance()->init();
-    _Init->getGraphicalInstances()[0]->getInstance()->displayObjects(GameSimulation(0));
-    _Init->getGraphicalInstances()[0]->getInstance()->displayScore(0);
-    getchar();
+    char keypressed = 0;
+    std::shared_ptr<IInit> _Init = std::make_shared<Init>("lib/");                              //create init class with the path to the libraries
+    std::cout << _Init->getGraphicalInstances()[0]->getInstance()->GetLibType() << std::endl;   //display lib type
+
+    _Init->getGraphicalInstances()[0]->getInstance()->InitWindow();                             //init window properties
+    do {
+        _Init->getGraphicalInstances()[0]->getInstance()->displayObjects(GameSimulation(0));    //display objects with their positions based on the simulation
+        _Init->getGraphicalInstances()[0]->getInstance()->displayScore(0);                      //display score
+        keypressed = getchar();                                                                 //get key pressed
+    } while (keypressed != 'q');                                                                //quit if q is pressed
 }
