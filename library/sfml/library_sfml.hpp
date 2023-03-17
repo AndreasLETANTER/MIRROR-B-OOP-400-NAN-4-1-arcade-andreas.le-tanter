298c3f3f5b80cac1782534a22110820b4af28f00
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-17 17:42:42 +0100

    [ADD]: temp library_sfml class and builded by cmake but work exactly like ncurses and display like ncurses

---
+++ library/sfml/library_sfml.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2023
+** BOOTSTRAP_Arcade [WSL : Ubuntu]
+** File description:
+** library_gra
+*/
+
+#pragma once
+#include "../IDisplayModule.hpp"
+#include <iostream>
+
+class LibrarySFML : public IDisplayModule {
+    public:
+        LibrarySFML();
+        ~LibrarySFML();
+        void InitWindow() override;
+        void FiniWindow() override;
+        void displayObjects(std::map<int, std::pair<ObjectType, std::pair<int, int>>> _ObjectData) override;
+        void displayScore(int _Score, int x, int y) override;
+        void displayText(std::string _String, std::pair<int, int> _Pos, Color FrontFont, Color BackFont) override;
+        const std::string &GetLibType() const;
+        std::pair<int, int> GetWindowSize() override;
+
+    protected:
+        int idx = 0;
+        WINDOW *_CurrentWindow;
+        std::map <Color, int> _ColorDefinition;
+    private:
+};
