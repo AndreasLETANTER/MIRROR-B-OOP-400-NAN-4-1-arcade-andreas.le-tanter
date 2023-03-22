a3c7ef779f5273cdef8ba838fbe4901036d65c04
HugoN1 <hugo.noury@epitech.eu>
2023-03-22 10:31:58 +0100

    [ADD]: Pacman class that inherits from game engine interface

---
+++ library/game_libraries/pacman/Pacman.hpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2023
+** arcade
+** File description:
+** handle Pacman
+*/
+#ifndef ARCADE_PACMAN_HPP
+    #define ARCADE_PACMAN_HPP
+    #include "../IGameEngine.hpp"
+
+class Pacman : public IGameEngine {
+public:
+    Pacman();
+    ~Pacman();
+    void handleUserInput(char key) override;
+    int getScore() const override;
+    bool getStatus() const override;
+    std::map<int, std::pair<ObjectType, std::pair<int, int>>> getObjects() const override;
+protected:
+    int _score;
+    bool _is_ended;
+    std::map<int, std::pair<ObjectType, std::pair<int, int>>> test; // <index, <ObjectType, <x, y>>>
+};
+#endif //ARCADE_PACMAN_HPP
