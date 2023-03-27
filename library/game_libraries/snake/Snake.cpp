ed4d260f0e58bd53ef45aec5576e43c85ef61f5b
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-03-27 14:00:58 +0200

    [ADD]: BoxCase of Snake game

---
+++ library/game_libraries/snake/Snake.cpp
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2023
+** MIRROR-B-OOP-400-NAN-4-1-arcade-andreas.le-tanter [WSL : Ubuntu]
+** File description:
+** snake
+*/
+
+#include "Snake.hpp"
+
+extern "C"
+{
+    IGameEngine *entryPoint()
+    {
+        return new Snake();
+    }
+}
+
+void Snake::CreateBoxCase(int x, int y, int x_length, int y_length)
+{
+    for (int i = x; i <= x_length + x; i++, last_idx++) {
+        _ObjectData[last_idx] = std::make_pair(Enum::ObjectType::BORDER, std::make_pair(i, y));
+        last_idx++;
+        _ObjectData[last_idx] = std::make_pair(Enum::ObjectType::BORDER, std::make_pair(i, y_length + y));
+    }
+    for (int i = y; i <= y_length + y; i++, last_idx++) {
+        _ObjectData[last_idx] = std::make_pair(Enum::ObjectType::BORDER, std::make_pair(x, i));
+        last_idx++;
+        _ObjectData[last_idx] = std::make_pair(Enum::ObjectType::BORDER, std::make_pair(x_length + x, i));
+    }
+}
+
+void Snake::handleUserInput(char key)
+{
+    (void)key;
+    CreateBoxCase(15, 1, 170, 50);
+}
+
+int Snake::getScore()
+{
+    return 0;
+}
+
+bool Snake::getStatus()
+{
+    return false;
+}
+
+Enum::libType Snake::GetLibType()
+{
+    return Enum::libType::GAME;
+}
+
+std::map<int, std::pair<Enum::ObjectType, std::pair<int, int>>> Snake::getObjects()
+{
+    return _ObjectData;
+}
---
+++ library/game_libraries/snake/Snake.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2023
+** MIRROR-B-OOP-400-NAN-4-1-arcade-andreas.le-tanter [WSL : Ubuntu]
+** File description:
+** snake
+*/
+
+#pragma once
+
+#include "../IGameEngine.hpp"
+
+class Snake : public IGameEngine {
+    public:
+        Snake() = default;
+        ~Snake() = default;
+        void handleUserInput(char key) override;
+        int getScore() override;
+        bool getStatus() override;
+        Enum::libType GetLibType();
+        std::map<int, std::pair<Enum::ObjectType, std::pair<int, int>>> getObjects() override;
+        
+    protected:
+        int _score;
+        bool _is_ended;
+        int last_idx = 0;
+        std::map<int, std::pair<Enum::ObjectType, std::pair<int, int>>> _ObjectData;
+    private:
+        void CreateBoxCase(int x, int y, int x_length, int y_length);
+};
