66cc3c1ae4e1d5b6e584368a6824452405c8fbed
hugon <hugo.noury@epitech.eu>
2023-04-05 15:19:58 +0200

    [ADD]: _GhostTimer and _GhostSpawnPos

--- library/game_libraries/pacman/Pacman.hpp
+++ library/game_libraries/pacman/Pacman.hpp
@@ -10,6 +10,7 @@
 #include "../IGameEngine.hpp"
 #include "../../ILibrary.hpp"
 #include <vector>
+#include <chrono>
 
 class Pacman : public IGameEngine {
 public:
@@ -27,11 +28,13 @@ protected:
     int _ObjectIndex = 0;
     int _MazeIndex = 0;
     bool _is_ended;
+    std::chrono::time_point<std::chrono::system_clock> _GhostTimer = std::chrono::system_clock::now() + std::chrono::seconds(10);
     std::pair<int, int> _MapBorderStartPos  = std::make_pair(60, 15);
     std::pair<int, int> _MapBorderSize = std::make_pair(37, 30);
     std::pair<int, int> _GhostSpawnAreaStartPos = std::make_pair(_MapBorderStartPos.first + _MapBorderSize.first / 2 - 7 / 2, _MapBorderStartPos.second + _MapBorderSize.second / 2 - 6 / 2);
     std::pair<int, int> _PacmanStartPos = std::make_pair(_GhostSpawnAreaStartPos.first + 3, _GhostSpawnAreaStartPos.second + 6);
     std::pair<Enum::ObjectType, std::pair<int, int>> _Pacman;
+    std::map<int, std::pair<int, int>> _GhostSpawnPos;
     std::map<int, std::pair<Enum::ObjectType, std::pair<int, int>>> _ObjectsData;
     std::map<int, std::pair<Enum::ObjectType, std::pair<int, int>>> _GumData; // <index, <Enum::ObjectType, <x, y>>>
     std::map<int, std::pair<Enum::ObjectType, std::pair<int, int>>> _GhostData;
