21ce8151bea7279d615e360f85aa6b06b46c949e
AndreasLETANTER <andreas.le-tanter@epitech.eu>
2023-04-05 15:33:06 +0200

    [FIX]: snake collision

--- library/game_libraries/snake/Snake.cpp
+++ library/game_libraries/snake/Snake.cpp
@@ -74,13 +74,10 @@ void Snake::CreateBoxCase(int x, int y, int x_length, int y_length)
 */
 bool Snake::CheckSnakeCollision(std::pair<int, int> player_pos)
 {
+    if (player_pos.first == STEP_X - 1 || player_pos.first == MAX_X + STEP_X - 1 || player_pos.second == STEP_Y + 1 || player_pos.second == MAX_Y + STEP_Y - 1)
+        return true;
     for (int i = 0; i < last_player_idx; i++) {
         if (_PlayerData[i].first == Enum::ObjectType::PLAYER_PART && ((_PlayerData[i].second.first == player_pos.first && _PlayerData[i].second.second == player_pos.second) || (_PlayerData[0].second.first == _ObjectData[i].second.first + 1 && _PlayerData[0].second.second == _ObjectData[i].second.second + 1))) {
-            return true;
-        }
-    }
-    for (int i = 0; i <= last_idx; i++) {
-        if (_ObjectData[i].first == Enum::ObjectType::BORDER && 0) {
             return true;
         }
     }
